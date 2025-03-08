ad3cff9f890edff65d38c980201b2c965d94e25f
Deepesh22148 <133644745+Deepesh22148@users.noreply.github.com>
2025-03-08 11:40:13 +0530

    Update page.tsx

--- Frontend/app/college-Info/page.tsx
+++ Frontend/app/college-Info/page.tsx
@@ -1,26 +1,11 @@
-import React from "react";
-import Navbar from "@/app/Components/navbar";
-import SideBar from "@/app/Components/sidebar";
+
+import Layout from "@/app/(root)/layout";
 import CollegePage from "@/app/college-Info/college_info";
 
-
-export default function CollegeInfoPage({ children }: { children: React.ReactNode }) {
+export default function CollegeInfoPage() {
     return (
-        <div className="flex min-h-screen">
-            {/* Sidebar */}
-            <div className="w-[300px] bg-gray-100">
-                <SideBar />
-            </div>
-
-            {/* Main Content Area */}
-            <div className="flex-1 flex flex-col">
-                <Navbar />
-                
-                {/* Ensure CollegePage is inside the main content */}
-                <div className="p-6">
-                    <CollegePage />
-                </div>
-            </div>
-        </div>
+        <Layout>
+            <CollegePage />
+        </Layout>
     );
 }
